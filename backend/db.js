8d1b05d70965d169215f046a9b0ab366c2a15eb6
Hector-Santizo <joelsantizo242@gmail.com>
2026-07-26 15:42:04 -0600

    se añadieron los recursos necesarios para la base de datos

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new SecurityPolicyViolationEvent({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    databe: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
