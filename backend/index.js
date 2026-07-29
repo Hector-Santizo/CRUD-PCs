7f8d0c91f4a46fde5cc144433e4634b850774619
Hector-Santizo <joelsantizo242@gmail.com>
2026-07-28 22:34:26 -0600

    Se añadieron comentarios para el mejor entendimiento del código

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
+//Exportacion de librerias
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 require('dotenv').config();
+
+//Middlewares
 const app = express();
 const PORT = 3000;
-
 app.use(cors());
 app.use(express.json());
+
+//Conexion de base de datos
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -25,6 +29,9 @@ app.get('/api/computadoras', async (req, res) => {
         res.status(500).json({ message: 'Error al obtener computadoras' })
     }
 });
+
+
+//Encendido de servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`)
 })
